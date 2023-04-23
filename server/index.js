58b704977998709e3f9ddc906a1f76f25236112a
lohpaul9 <lohpaul9@seas.upenn.edu>
2023-04-22 23:04:36 -0400

    attempt to deploy server netlify

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 var cors = require('cors')
 const bodyParser = require('body-parser');
 const { fetchByWeek } = require('./upload');
@@ -7,7 +8,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.post('/', async (req, res) => {
+router.post('/', async (req, res) => {
   date = req.body.date;
   console.log(date)
   urls = await fetchByWeek(date);
@@ -17,6 +18,12 @@ app.post('/', async (req, res) => {
   }
   res.send(urls);
 });
+
+router.get('/', async (req, res) => {
+  res.send({ hello: "Hello" });
+});
+
+app.use('/.netlify/functions/api', router);
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
